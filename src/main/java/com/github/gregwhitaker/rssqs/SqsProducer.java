1115913c312e93696345349450508426b0f140dd
Whitaker, Greg <Greg.Whitaker@nike.com>
2016-08-10 16:14:59 -0700

    Implemented the SQS Producer

--- src/main/java/com/github/gregwhitaker/rssqs/SqsProducer.java
+++ src/main/java/com/github/gregwhitaker/rssqs/SqsProducer.java
@@ -16,16 +16,70 @@
 
 package com.github.gregwhitaker.rssqs;
 
-public class SqsProducer {
-    private final String queueArn;
-    private final String region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.sqs.model.GetQueueUrlResult;
+import com.github.gregwhitaker.rxsqs.ReactiveSqsClient;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+import rx.Observable;
+import rx.RxReactiveStreams;
 
-    public SqsProducer(final String queueArn, final String region) {
-        this.queueArn = queueArn;
-        this.region = region;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+public class SqsProducer {
+    private final String queueName;
+    private final ReactiveSqsClient rxSqsClient;
+
+    public SqsProducer(final String queueName, final String region) {
+        this.queueName = queueName;
+        this.rxSqsClient = new ReactiveSqsClient(Regions.fromName(region));
     }
 
     public void start() {
+        rxSqsClient.getQueueUrlAsync(queueName)
+                .last()
+                .map(GetQueueUrlResult::getQueueUrl)
+                .subscribe(url -> {
+                    CountDownLatch latch = new CountDownLatch(Integer.MAX_VALUE);
 
+                    // Observable that emits a message every second
+                    RxReactiveStreams
+                            .toPublisher(Observable
+                                    .interval(1_000, TimeUnit.MILLISECONDS)
+                                    .map(i -> String.format("This is message %s", i)))
+                            .subscribe(new Subscriber<String>() {
+                                @Override
+                                public void onSubscribe(Subscription s) {
+                                    s.request(Long.MAX_VALUE);
+                                }
+
+                                @Override
+                                public void onNext(String s) {
+                                    // Take the message and put it on the SQS queue
+                                    rxSqsClient.sendMessageAsync(url, s)
+                                            .subscribe(result -> {
+                                                latch.countDown();
+                                            });
+                                }
+
+                                @Override
+                                public void onError(Throwable t) {
+                                    t.printStackTrace();
+                                    latch.countDown();
+                                }
+
+                                @Override
+                                public void onComplete() {
+                                    latch.countDown();
+                                }
+                            });
+
+                    try {
+                        latch.await();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                });
     }
 }
